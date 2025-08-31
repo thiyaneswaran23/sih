5b2377d2ada912c74416f73c22aba25a4ec6a8e1
thanos2301 <thiyaneswaran107@gmail.com>
2025-08-31 19:02:02 +0530

    setup env

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("MONGODB CONNECTED"))
+.catch(err => console.error("ERROR", err));
+
+app.get("/", (req, res) => {
+    res.send("API is Working");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
