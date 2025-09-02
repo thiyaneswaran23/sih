69cdc67c92b4a00e1fcff122d8de52b01a1a4a1a
thanos2301 <thiyaneswaran107@gmail.com>
2025-09-02 23:00:22 +0530

    Mock landing page created

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,5 @@ mongoose.connect(process.env.MONGO_URI, {
 app.get("/", (req, res) => {
     res.send("API is Working");
 });
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
